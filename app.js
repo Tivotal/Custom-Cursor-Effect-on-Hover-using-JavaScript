5edab8228368c86c2d35e10ed5208cc3faaef8dc
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-06-14 07:43:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+/* Created by Tivotal */
+
+let text = document.querySelector(".text");
+let pointer = document.querySelector(".pointer");
+
+const movePointer = (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  pointer.style.left = x + "px";
+  pointer.style.top = y + "px";
+
+  let rect = text.getBoundingClientRect();
+
+  if (x > rect.left && x < rect.right && y > rect.top && y < rect.bottom) {
+    pointer.style.width = 100 + "px";
+    pointer.style.height = 100 + "px";
+  } else {
+    pointer.style.width = 30 + "px";
+    pointer.style.height = 30 + "px";
+  }
+};
+
+document.addEventListener("mousemove", (e) => {
+  movePointer(e);
+});
